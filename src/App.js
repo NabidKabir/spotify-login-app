6452f4805c936b1e5c533e5e845446cf74a3c7b1
NabidKabir <nabid.kabir312@gmail.com>
2023-07-11 19:44:20 -0400

    added some stuff idk still doesnt work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {Container, Button } from 'react-bootstrap';
+
+const clientID = process.env.REACT_APP_CLIENT_ID;
+const clientSECRET = process.env.REACT_APP_CLIENT_SECRET;
+const redirectURI = 'http://localhost:3000/callback'
+
+const scopes = ['user-read-currently-playing'];
+
+window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${encodeURIComponent(redirectURI)}&scope=${encodeURIComponent(scopes.join(' '))}&response_type=code`;
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-    
+      <Container className='d-flex align-items-center justify-content-center' style={{minHeight:'100vh'}}>
+        <Button className='btn btn-success'>Login</Button>
+      </Container>
     </div>
   );
 }
