1a42c6f3f97903b73e46e51abe3d4d4358376864
NabidKabir <nabid.kabir312@gmail.com>
2023-08-03 02:00:30 -0400

    figured out redirectURI stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import {Container, Button } from 'react-bootstrap';
+import {useEffect, useState} from 'react';
 
-const clientID = process.env.REACT_APP_CLIENT_ID;
-const clientSECRET = process.env.REACT_APP_CLIENT_SECRET;
-const redirectURI = 'http://localhost:3000/callback'
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+const REDIRECT_URI = 'http://localhost:3000'
+const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize'
+const RESPONSE_TYPE = 'token'
 
 const scopes = ['user-read-currently-playing'];
 
-window.location.href = `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${encodeURIComponent(redirectURI)}&scope=${encodeURIComponent(scopes.join(' '))}&response_type=code`;
+
+
 
 
 
 
 
 function App() {
+  const [token, setToken] = useState('')
+
+  const getToken = () => {
+    var urlParams = new URLSearchParams(window.location.hash.replace('#', '?'))
+    return urlParams.get('access_token')
+  }
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+
+    if (!token && hash) {
+        token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+        window.location.hash = ""
+        window.localStorage.setItem("token", token)
+    }
+
+    setToken(token)
+  }, [])
+
+  const logout = () => {
+    setToken('')
+    window.localStorage.removeItem('token')
+  }
+
+  console.log(token)
+
   return (
     <div className="App">
       <Container className='d-flex align-items-center justify-content-center' style={{minHeight:'100vh'}}>
-        <Button className='btn btn-success'>Login</Button>
+          {!token ?
+          <Button 
+            href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${scopes}`} 
+            className='btn btn-success'>Login</Button>
+          : <Button 
+            onClick={logout}
+            className='btn btn-danger'>Logout</Button>}
       </Container>
     </div>
   );
